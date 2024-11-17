2510d8b81721ef7e2465f666cbc6db1df7691295
Darya Jenkins <melissegreenleaf@gmail.com>
2024-11-17 18:20:20 +0100

    first try

--- src/main/java/ru/netology/javaqa/javaqamvn/oop/Radio.java
+++ src/main/java/ru/netology/javaqa/javaqamvn/oop/Radio.java
@@ -1,4 +1,54 @@
 package ru.netology.javaqa.javaqamvn.oop;
 
 public class Radio {
+    private int currentVolume;
+    private int currentStation;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void nextStation() {
+        currentStation = (currentStation == maxStation) ? minStation : currentStation + 1;
+    }
+
+    public void prevStation() {
+        currentStation = (currentStation == minStation) ? maxStation : currentStation - 1;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {
+        if (newCurrentStation > maxStation) {
+            return;
+        }
+        if (newCurrentStation < minStation) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume--;
+        }
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
 }
--- src/test/java/ru/netology/javaqa/javaqamvn/oop/RadioTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/oop/RadioTest.java
@@ -1,4 +1,41 @@
 package ru.netology.javaqa.javaqamvn.oop;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    public void switchToNextStation() {
+        radio.setCurrentStation(9);
+        radio.nextStation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPreviousStation() {
+        radio.setCurrentStation(0);
+        radio.prevStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,0", "1,1", "8,8", "9,9", "10,0"
+    })
+    public void testBoundariesStations(int setCurrentStation, int expected) {
+        int actual = radio.getCurrentStation();
+        Assertions.assertEquals(expected, actual);
+    }
 }
